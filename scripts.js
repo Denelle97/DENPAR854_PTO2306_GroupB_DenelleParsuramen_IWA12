054942c262b5b3fae1da21546eaa01512ec2d87d
Denelle97 <denelle728@gmail.com>
2023-11-05 16:26:26 +0200

    challenge solved

--- scripts.js
+++ scripts.js
@@ -25,37 +25,117 @@ const STATUS_MAP = {
     }
 }
 
-// Edit below line 
+// Helper function to find elements by ID and class
+const selector = (id, className) => document.querySelector(`#${id} .${className}`);
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+/**
+ * Gets references to the status, reserve, checkout, and checkin elements for a book.
+ *
+ * @param {string} bookId - The ID of the book.
+ * @returns {Object} An object containing references to book elements.
+ */
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const getBookElements = (bookId) => {
+  const book = selector(bookId, "status");
+  const reserve = selector(bookId, "reserve");
+  const checkout = selector(bookId, "checkout");
+  const checkin = selector(bookId, "checkin");
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+  return { book, reserve, checkout, checkin };
+};
 
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// Changed variable names to be more descriptive (reserve, checkout, checkin)
+const book1 = getBookElements("book1");
+const book2 = getBookElements("book2");
+const book3 = getBookElements("book3");
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+/**
+ * Updates the status color for a book based on the STATUS_MAP.
+ *
+ * @param {Object} book - The book object containing references to book elements.
+ * @param {string} status - The status of the book.
+ */
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+const updateStatusColor = (book, status) => {
+  const color = STATUS_MAP[status].color;
+  book.style.color = color;
+};
+
+/**
+ * Updates the button status for a book based on the STATUS_MAP.
+ *
+ * @param {Object} book - The book object containing references to book elements.
+ * @param {string} status - The status of the book.
+ */
+
+const updateButtonStatus = (book, status) => {
+  const { canReserve, canCheckout, canCheckIn } = STATUS_MAP[status];
+  // Changed variable names to be more descriptive (reserve, checkout, checkin)
+  book.reserve.disabled = !canReserve;
+  book.checkout.disabled = !canCheckout;
+  book.checkin.disabled = !canCheckIn;
+};
+
+// Updates status color and button status for each book
+updateStatusColor(book1.book, book1.book.textContent);
+updateButtonStatus(book1, book1.book.textContent);
+
+updateStatusColor(book2.book, book2.book.textContent);
+updateButtonStatus(book2, book2.book.textContent);
+
+updateStatusColor(book3.book, book3.book.textContent);
+updateButtonStatus(book3, book3.book.textContent);
+
+// Additional adjustments
+book1.checkin.style.color = ""; 
+book2.checkin.style.color = ""; // Changed the color of the buttons in the DOM to the correct colors
+book3.checkin.style.color = "";
+
+// Function calls
+statusColorController();
+buttonController();
+
+
+//////////////////////////////////////////////////////////////
+//This is another way it could have been resolved but ran into too many bugs 
+
+
+// Function to get elements by class name
+// function selector(className) {
+//     return document.getElementsByClassName(className);
+//   }
+  
+//   // Update for book1
+//   const status1 = selector("status")[0];
+//   const reserve1 = selector("reserve")[0];
+//   const checkout1 = selector("checkout")[0];
+//   const checkin1 = selector("checkin")[0];
+  
+//   status1.style.color = STATUS_MAP[status1.textContent].color;
+//   reserve1.disabled = !STATUS_MAP[status1.textContent].canReserve;
+//   checkout1.disabled = !STATUS_MAP[status1.textContent].canCheckout;
+//   checkin1.disabled = !STATUS_MAP[status1.textContent].canCheckIn;
+  
+//   // Update for book2
+//   const status2 = selector("status")[1];
+//   const reserve2 = selector("reserve")[1];
+//   const checkout2 = selector("checkout")[1];
+//   const checkin2 = selector("checkin")[1];
+  
+//   status2.style.color = STATUS_MAP[status2.textContent].color;
+//   reserve2.disabled = !STATUS_MAP[status2.textContent].canReserve;
+//   checkout2.disabled = !STATUS_MAP[status2.textContent].canCheckout;
+//   checkin2.disabled = !STATUS_MAP[status2.textContent].canCheckIn;
+  
+//   // Update for book3
+//   const status3 = selector("status")[2];
+//   const reserve3 = selector("reserve")[2];
+//   const checkout3 = selector("checkout")[2];
+//   const checkin3 = selector("checkin")[2];
+  
+//   status3.style.color = STATUS_MAP[status3.textContent].color;
+//   reserve3.disabled = !STATUS_MAP[status3.textContent].canReserve;
+//   checkout3.disabled = !STATUS_MAP[status3.textContent].canCheckout;
+//   checkin3.disabled = !STATUS_MAP[status3.textContent].canCheckIn;
+
+//I could also use boolean (IF) to solve this challenge 
